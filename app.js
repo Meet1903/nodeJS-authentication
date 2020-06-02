3b1a453c4a50a45241e2c3ed18942603d620235c
Meet1903 <meet.diwan.md@github.com>
2020-06-03 00:08:13 +0530

    openssl configuration

--- app.js
+++ app.js
@@ -26,6 +26,16 @@ const connect = mongoose.connect(url);
 
 var app = express();
 
+app.all('*', (req, res, next) => {
+	if(req.secure){
+		return next();
+	}
+	else{
+		//307 is the status code.
+		res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+	}
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
